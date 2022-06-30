29ba410b4c46d38fd2472602acf706864f2bc9cb
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-06-30 03:10:54 -0700

    message:c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
- */
-int main(void)  
-{
-  puts("\"Programming is like building a multilingual puzzle");
+ **/
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
