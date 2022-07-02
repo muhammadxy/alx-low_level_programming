bd0dd10b8aba6fbd8ea52a8f737b1c4af7b60712
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-01 20:49:40 -0700

    message:c programming

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,14 +7,11 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{int i = 0;
-while (i < 48)
-if (i < 10)
-putchar(i + '0');
-else if (i > 41)
-putchar(i - 10 + 'A');
-i++;
-}
-putchar(10);
+{int al, an;
+for (al = '0'; al <= '9'; al++)
+putchar(al);
+for (an = 97; an <= 102; an++)
+putchar(an);
+putchar('\n');
 return (0);
 }
