8a02903674afc47ff07ce3a2768f5c6cb6d13506
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-01 17:08:35 -0700

    message:c programming

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - print single digit numbers
+*
+* Description: print single digit number
+*
+* Return: Always 0 (Success)
+*/
+int main(void) 
+{int i;
+i = 48;
+while (i < 58)
+{
+putchar(i);i++;
+}  
+putchar(10);
+return (0);
+}
