11507e36e99c214093b53f91f63b2435ea96df9f
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-07 12:45:50 -0700

    message:more nested function

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+* print_diagonal - function outputs \
+*
+*@n: is int
+*
+* Return: 0
+*/
+void print_diagonal(int n)
+{
+int row, space;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= n; row++)
+{
+for (space = 1; space <= n; space++)
+{
+if (space < row)
+{
+_putchar(' ');
+}
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
