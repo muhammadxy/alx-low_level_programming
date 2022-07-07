8ddc15e13c538ae95997c0110f6b909cd741dd1b
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-07 13:17:29 -0700

    message:more nested function

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* print_triangle - function outputs triangle #
+*
+*@size: is int
+*
+* Return: 0
+*/
+void print_triangle(int size)
+{
+int row, space;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 0; row < size; row++)
+{
+for (space = (size - 1); space >= 0; space--)
+{
+if (space <= row)
+{
+_putchar('#');
+}
+else
+{
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
+}
