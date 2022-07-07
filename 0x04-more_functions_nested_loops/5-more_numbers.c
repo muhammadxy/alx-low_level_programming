5b6a2d6c898e836fb99f374da156abc15bbf4dcb
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-07 12:25:37 -0700

    message:more nested function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* more_numbers - function outputs digits 0-14 ten times
+*
+*
+* Return: 0
+*/
+void more_numbers(void)
+{
+int num, col;
+for (col = 0; col <= 9; col++)
+{
+for (num = 0; num <= 14; num++)
+{
+if (num >= 10)
+{
+_putchar((num / 10) + '0');
+}
+_putchar((num % 10) + '0');
+}
+_putchar('\n');
+}
+}
