bed719330f295a3dd2a902a35d6a25bd9032587f
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-12 06:42:52 -0700

    message:pointer and arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+* puts_half - function prints half of a string
+*
+*@str: char pointer to string
+*
+* Return: 0
+*/
+void puts_half(char *str)
+{
+int len = _strlen(str);
+int y;
+if (len % 2 == 0)
+{
+y = len / 2;
+}
+else if (len % 2 != 0)
+{
+y = (len + 1) / 2;
+}
+for (; *(str + y) != '\0'; y++)
+{
+_putchar(*(str + y));
+}
+_putchar('\n');
+}
+/**
+* _strlen - function returns length of a string
+*
+*@s: char pointer
+*
+* Return: length
+*
+*/
+int _strlen(char *s)
+{
+char *copy_s = s;
+int length = 0;
+while (*copy_s != '\0')
+{
+length++;
+copy_s++;
+}
+return (length);
+}
