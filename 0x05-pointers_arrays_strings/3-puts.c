2b51ddbb12646ce99388840038c35468e4ccf497
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-12 05:50:38 -0700

    message:pointer and arrays

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _puts - function prints a string
+*
+*@str: char pointer
+*
+* Return: 0
+*/
+void _puts(char *str)
+{
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
