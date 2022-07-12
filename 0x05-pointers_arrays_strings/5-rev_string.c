691e8f692c041053a1025243bff9969066af7541
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-12 06:16:45 -0700

    message:pointer and arrays

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+* rev_string - function reverses a string
+*
+*@s: char pointer
+*
+* Return: *s
+*/
+void rev_string(char *s)
+{
+int len = _strlen(s);
+int mid = len / 2;
+int count = 0;
+char tmp;
+while (count < mid)
+{
+tmp = s[count];
+s[count] = s[len - count - 1];
+s[len - count - 1] = tmp;
+count++;
+}
+}
+/**
+* _strlen - function returns length of a string
+*
+*@s: char pointer
+*
+* Return: length
+*
+*/
+int _strlen(char *s)
+{
+char *copy_s = s;
+int length = 0;
+while (*copy_s != '\0')
+{
+length++;
+copy_s++;
+}
+return (length);
+}
