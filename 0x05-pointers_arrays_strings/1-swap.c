8d6fce8fcf78c74fb13bc78006ee3d35aaf45aae
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-12 05:32:33 -0700

    message:swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* swap_int - function swaps int variable values around
+*
+*@a: int pointer
+*@b: int pointer
+*
+* Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+int var;
+var = *a;
+*a = *b;
+*b = var;
+}
