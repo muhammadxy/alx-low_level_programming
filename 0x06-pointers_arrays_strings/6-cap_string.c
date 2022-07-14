c1c72ecd76609994973987c03d120b835922cecd
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-14 06:46:16 -0700

    message:pointers arrays

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdio.h>
+/**
+* *cap_string - function capitalizes all words of a string
+*
+*@n: char * pointer
+*
+* Return: char *
+*/
+char *cap_string(char *n)
+{
+int count;
+int count2;
+char *x = " .,{}()\n\t\"?!";
+for (count = 0; *(n + count) != '\0'; count++)
+{
+for (count2 = 0; *(x + count2) != '\0'; count2++)
+{
+if (*(n + count - 1) == *(x + count2) && *(n + count) >=
+'a' && *(n + count) <= 'z')
+{
+*(n + count) = ('A' - 'a') + *(n + count);
+}
+else if ((count == 0) && *(n + count) >= 'a' && *(n + count) <= 'z')
+{
+*(n + count) = ('A' - 'a') + *(n + count);
+}
+}
+}
+return (n);
+}
