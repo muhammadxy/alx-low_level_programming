8351c226392300883e8b233230909d4d3f3fa7d0
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-14 09:41:16 -0700

    message:rot13

---
+++ 0x06-pointers_arrays_strings/100-rot1.c
@@ -0,0 +1,70 @@
+
+
+#include<stdio.h>
+
+#define ROT 13
+
+
+char *rot13(const char *src)  
+{
+  
+  int c,e;
+  
+
+  
+  while((c=getchar())!=EOF)
+    
+    {
+      
+      if(c >='A' && c <='Z')
+	
+        {
+	  
+	  if((e = c + ROT) <= 'Z')
+	    
+	    putchar(e);
+	  
+	  else
+	    
+            {
+	      
+	      e = c - ROT;
+	      
+	      putchar(e);
+	      
+            }
+	  
+        }
+      
+      else if(c >='a' && c <='z')
+	
+        {
+	  
+	  if((e= c + ROT) <= 'z')
+	    
+	    putchar(e);
+	  
+	  else
+	    
+            {
+	      
+	      e = c - ROT;
+	      
+	      putchar(e);
+	      
+            }
+	  
+        }
+      
+      else
+	
+	putchar(c);
+      
+    }
+  
+
+  
+  return 0;
+  
+}
+
---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *
+ROT13 
+*
+*/
+char *rot13(char *start){
+  
+  int i, len = strlen(start);
+  
+  for (i=0; i<len; i++)
+    
+    {
+      
+      if( (*(start+i)>='a' && *(start+i)<'n') || (*(start+i)>='A' && *(start+i)<'N') )
+*(start + i) += 13;
+ else if ( (*(start+i)>'m' && *(start+i)<'z') || (*(start+i)>'M' && *(start+i)<'Z') )
+	
+	*(start +i) -= 13;
+      
+    }
+  return (0);  
+}
