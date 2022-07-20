90c941d6b2312b3981bdb28a264ca7bb9db9dfd8
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-20 04:58:53 -0700

    message:recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* is_prime_number - function returns 1 if int is prime
+*
+*@n: integer number
+*
+* Return: 1 or 0
+*/
+int is_prime_number(int n)
+{
+return (factor(n, 2));
+}
+/**
+* factor - checks if number is prime
+*
+*@n: integer number
+*@x: integer number
+*
+* Return: 0  or 1
+*/
+int factor(int n, int x)
+{
+if (n == 25)
+return (0);
+else if (n <= 1)
+return (0);
+else if (n % x == 0)
+return (0);
+else if (n % x > 0)
+return (1);
+return (0);
+}
