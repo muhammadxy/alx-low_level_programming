af9af0a2264eb569c615e8467b31d041ba982ca5
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-22 09:13:07 -0700

    message:argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - function multiplies two numbers
+*
+*@argc: int
+*@argv: char * pointer to array
+*
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+int multi = 0;
+if (argc == 3)
+{
+multi += atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", multi);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
