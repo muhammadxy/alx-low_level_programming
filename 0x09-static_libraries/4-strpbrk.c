8f108f0fdd6bbf80dc09f20886825d3b5aa4acf0
Ayuba Muhammad <muhammadayubaxy@gmail.com>
2022-07-22 03:48:37 -0700

    message:static library

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* *_strpbrk - function searches string for set of bytes*
+*@s: char * pointer
+*@accept: char * pointer
+* Return: s
+*/
+char *_strpbrk(char *s, char *accept)
+{
+char *ap = accept;
+int counter = 0;
+while (*s)
+{
+while (*(ap + counter))
+{
+if (*s == *(ap + counter))
+{
+return (s);
+}
+counter++;
+}
+counter = 0;
+s++;
+}
+return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* *_strcpy - function copies string pointed by src to buffer pointer dest
+*
+*@dest: char pointer
+*@src: char pointer
+*
+* Return: dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+char *pdest = dest;
+while (*src != '\0')
+{
+*pdest = *src;
+src++;
+pdest++;
+}
+return (dest);
+}
